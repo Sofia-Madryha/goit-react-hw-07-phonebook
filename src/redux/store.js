ae6e013f7e3f30b6f48206da1721bfed0e3ed25d
Sofia-Madryha <sonyamadryha3@gmail.com>
2023-11-22 01:13:16 +0200

    fix

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,16 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit";
-import {  contactReducer, persistedContactsReducer } from "./contactSlice";
+import {  contactReducer} from "./contactSlice";
 // import { filterReducer } from "./filterSlice";
-import {
-    persistStore,
-    FLUSH,
-    REHYDRATE,
-    PAUSE,
-    PERSIST,
-    PURGE,
-    REGISTER,
-  } from 'redux-persist';
-import { filterReducer } from "./filterSlice";
+// import {
+//     persistStore,
+//     FLUSH,
+//     REHYDRATE,
+//     PAUSE,
+//     PERSIST,
+//     PURGE,
+//     REGISTER,
+//   } from 'redux-persist';
+// import { filterReducer } from "./filterSlice";
 
 
 
@@ -19,12 +19,12 @@ export const store = configureStore({
       contacts: contactReducer,
       // filters: filterReducer,
     },
-    middleware: getDefaultMiddleware =>
-    getDefaultMiddleware({
-      serializableCheck: {
-        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
-      },
-    }),
+    // middleware: getDefaultMiddleware =>
+    // getDefaultMiddleware({
+    //   serializableCheck: {
+    //     ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+    //   },
+    // }),
   });
 
-  export const persistor = persistStore(store);
+  // export const persistor = persistStore(store);
